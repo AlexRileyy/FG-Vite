a91d2791e0f122422b6afa0659803fa7a5868585
Your Name <you@example.com>
2024-06-13 16:27:06 +0100

    made changes to the app - ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <title>Forrest Gump</title>
+      
+      
+      <h1>Forrest Gump</h1>
+      <h2>Starring Tom Hanks</h2>
+      <h3>Date: 1994</h3>
+    
+      <img src="https://i.ytimg.com/vi/187trX0mo1o/maxresdefault.jpg" alt="Forrest Gump on his long run." />
+        <p>"Forrest Gump" is a 1994 American drama film directed by Robert Zemeckis, based on the 1986 novel by Winston Groom. The story follows the life of Forrest Gump, a kind-hearted man with a low IQ, played by Tom Hanks. Despite his cognitive limitations, Forrest witnesses and unwittingly influences several defining historical events in the 20th century United States, all while pursuing his deep love for his childhood friend, Jenny. The film explores themes of destiny, love, and the impact of historical moments on individual lives, all underscored by Forrest's unwavering optimism and integrity.</p>
+    
+    
+        
+    
     </>
   )
 }
